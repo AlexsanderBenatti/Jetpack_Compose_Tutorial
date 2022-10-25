7b0436f932352023be07b8f83f1d8ea4ed23ed35
AlexsanderBenatti <abenatti.silva@gmail.com>
2022-10-25 10:57:51 -0300

    No unecessary imports

--- app/src/main/java/com/example/composertutorial/MainActivity.kt
+++ app/src/main/java/com/example/composertutorial/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.example.composertutorial
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composertutorial.ui.theme.ComposerTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
