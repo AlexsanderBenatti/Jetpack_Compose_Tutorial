d68d87c03e00ca7733640accf33f9e718ad703e1
AlexsanderBenatti <abenatti.silva@gmail.com>
2022-10-31 08:38:03 -0300

    First commit, thrid project

--- app/src/main/java/com/example/composertutorial/MainActivity.kt
+++ app/src/main/java/com/example/composertutorial/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composertutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,20 +9,30 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.composertutorial.ui.theme.ComposerTutorialTheme
 
+//Adendo: Algumas linhas eu não comentei porque as funções delas já estão comentadas
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { //Layout
-            MessageCard(Message("Android", "Jetpack Compose")) //Função de texto pré-definido
-            //Autor, Mensagem
+            ComposerTutorialTheme(darkTheme = true) { //Mudando o tema para modo escuro
+                Surface(modifier = Modifier.fillMaxSize()) { //Alterando o layout da mensagem pra maximizar o width e o height
+                    MessageCard(Message("Alexsander", "Meu Jetpack Compose funcionou :D")) //Função de texto pré-definido
+                    //Autor, Mensagem
+                }
+            }
         }
     }
 }
@@ -30,30 +41,53 @@ data class Message(val author: String, val body: String) //Declarando um objeto 
 
 @Composable //Anotação pra tornar uma função composta
 fun MessageCard(msg: com.example.composertutorial.Message) { //Função do texto pré-definido
-    // Add padding around our message
     Row(modifier = Modifier.padding(all = 8.dp)) { //Adicionando padding em volta da mensagem
-        Image(
+        Image( //Adicionando a imagem
             painter = painterResource(R.drawable.profile_picture), //Escolhendo qual imagem usar
             contentDescription = "Contact profile picture", //Descrição da imagem
             modifier = Modifier //Modificando a forma da imagem
                 .size(40.dp) //Mudando o tamanho
                 .clip(CircleShape) //Deixando em formato de círculo
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape) //Adicionando uma borda a imagem
         )
 
-        // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp)) //Espaço entre a imagem e coluna
         Column { //Coluna
-            Text(text = msg.author) //Printando primeiro o autor
+            Text( //Printando primeiro o autor
+                text = msg.author, //Definindo qual vai ser o nome do autor (pegando o msg.author da classe Message)
+                color = MaterialTheme.colors.secondary, //Definindo a cor do texto
+                style = MaterialTheme.typography.subtitle1 //Definindo o tamanho do texto
+            )
             Spacer(modifier = Modifier.height(4.dp)) //Espaço entre os elementos da coluna
-            Text(text = msg.body) //Printando a mensagem
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) { //Mudando o layout do texto
+                Text( //Printando a mensagem
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body1
+                )
+            }
         }
     }
 }
 
-@Preview //Anotação pra definir uma função para preview (ela não aparece no projeto)
+@Preview( //Anotação para visualizar uma prévia do projeto no modo claro
+    uiMode = Configuration.UI_MODE_NIGHT_NO, //Settando para o modo claro
+    showBackground = true, //Usar uma cor de background padrão
+    name = "Light Mode" //Nomeando a preview
+)
+@Preview( //Anotação para visualizar uma prévia do projeto no modo escuro (em relação a função abaixo)
+    uiMode = Configuration.UI_MODE_NIGHT_YES, //Settando para o modo escuro
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!") //Usando outro tipo de mensagem para o preview
-    )
+    ComposerTutorialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(
+                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!") //Testando outra mensagem para o preview
+            )
+        }
+    }
+
 }
